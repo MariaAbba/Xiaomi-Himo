ce33ba6dff19c6594093e50cd461c22855493fba
Maria <99909488+MariaAbba@users.noreply.github.com>
2023-03-01 18:23:01 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+    $('.bike-slider, .slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    // $(".menu, .bike__column").on("click", "a", function (event) {
+    //     event.preventDefault();
+    //     var id = $(this).attr('href'),
+    //         top = $(id).offset().top;
+    //     $('body,html').animate({ scrollTop: top }, 1500);
+    //     14
+    // });
+});
